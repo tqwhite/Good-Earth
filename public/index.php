c7190a7783579113a37a395f69077f986b29ec90
TQ White II/qbook <tq@justkidding.com>
2012-08-17 12:23:43 -0500

    revised index.php for readability

--- public/index.php
+++ public/index.php
@@ -2,19 +2,30 @@
 
 define('DOCROOT_DIRECTORY_PATH', realpath(dirname(__FILE__)).'/'); //needed for AJAX apps
 
-// Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+if (!defined('APPLICATION_PATH')){
+	define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+}
 
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+if (!defined('APPLICATION_ENV')){
+	if (getenv('APPLICATION_ENV')){
+		$environmentName=getenv('APPLICATION_ENV');
+	}
+	else{
+		$environmentName='production';
+	}
+    define('APPLICATION_ENV', $environmentName);
+}
 
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
+set_include_path(
+	implode(
+		PATH_SEPARATOR,
+		array(
+			realpath(APPLICATION_PATH . '/../library'),
+			get_include_path(),
+		)
+	)
+);
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
