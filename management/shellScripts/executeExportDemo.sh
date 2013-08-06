1bf3c2385da64b216c1b89522d5602bcc5c65c1d
TQ White II <tq@justkidding.com>
2013-08-05 22:50:55 -0500

    production export script

--- management/shellScripts/executeExportDemo.sh
+++ management/shellScripts/executeExportDemo.sh
@@ -3,6 +3,6 @@
 backupDir="/home/websites/com.genericwhite.goodearth"
 dirname="backup_"`eval date +%Y%m%d%H%M`
 mkdir $backupDir/backups/$dirname
-mysqldump -ugoodearthsite -pglory*snacks  goodEarthDemoData > $backupDir/backups/$dirname/goodEarthProductionData.sql
+mysqldump -ugoodearthsite -pglory*snacks  goodEarthDemoData > $backupDir/backups/$dirname/goodEarthDemoData.sql
 
 curl -k http://goodearth.genericwhite.com/data/export > $backupDir/backups/$dirname/exportTranscript.html
--- management/shellScripts/executeExportDev.sh
+++ management/shellScripts/executeExportDev.sh
@@ -3,6 +3,6 @@
 backupDir="/home/tqwhite/clients/goodearth"
 dirname="backup_"`eval date +%Y%m%d%H%M`
 mkdir $backupDir/backups/$dirname
-mysqldump -utqorg -pmoney*pie  goodearthStore > $backupDir/backups/$dirname/goodEarthProductionData.sql
+mysqldump -utqorg -pmoney*pie  goodearthStore > $backupDir/backups/$dirname/goodEarthDevData.sql
 
 curl http://store.goodearth.local/data/export > $backupDir/backups/$dirname/exportTranscript.html
