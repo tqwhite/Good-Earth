913525ee44a07e7bd72427abb77114a41a3ca8ca
TQ White II <tq@justkidding.com>
2013-08-05 22:15:31 -0500

    production export script

--- management/shellScripts/executeExportDemo.sh
+++ management/shellScripts/executeExportDemo.sh
@@ -5,4 +5,4 @@ dirname="backup_"`eval date +%Y%m%d`
 mkdir $backupDir/backups/$dirname
 mysqldump -ugoodearthsite -pglory*snacks  goodEarthDemoData > $backupDir/backups/$dirname/goodEarthProductionData.sql
 
-curl https://sales.goodearthorganiclunches.com/data/export > $backupDir/backups/$dirname/exportTranscript.html
+curl -k https://sales.goodearthorganiclunches.com/data/export > $backupDir/backups/$dirname/exportTranscript.html
