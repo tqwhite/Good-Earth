28a1edfc8646ef1112a33fb3c61c2d35f14318cb
TQ White II <tq@justkidding.com>
2017-05-28 15:55:53 -0500

    removed submit button repeat lockout temporarily

--- public/js/good_earth_store/controller/customer/checkout/checkout.js
+++ public/js/good_earth_store/controller/customer/checkout/checkout.js
@@ -117,7 +117,8 @@ submitButtonHandler:function(control, parameter){
 	switch(control){
 		case 'click':
 
-			if (this.displayParameters.submitButton.submitted){return;}
+	//		if (this.displayParameters.submitButton.submitted){return;}
+	console.log("SUBMIT BUTTON LOCKOUT DISABLED IN /controller/customer/checkout/checkout.js");
 			
 			$('#'+this.displayParameters.submitButton.divId).html('Processing');
 			this.displayParameters.submitButton.submitted=true;
